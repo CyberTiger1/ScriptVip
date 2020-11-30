1d91b720936479aeef8cc87d051a6a6695fc81eb
꧁☆☬₥Ɽ.Vł₵₮ØⱤ☬☆꧂ <74651880+CyberTiger1@users.noreply.github.com>
2020-11-30 17:51:08 +0700

    Update scvp.sh

--- scvp.sh
+++ scvp.sh
@@ -10,7 +10,6 @@ echo "                   -MR.CLONE"
 echo "                   -Mr Fahri Firmansyah"
 echo "                   -꧁☬☆༺ঔৣ>²֎M⃟r̰̃•²-R f l Y²<ঔৣ  •𖣘•¹Iཱུ  ༆༻☬☆꧂"
 echo "                   -Mr Goat"
-echo "                   -Mr Zamal"
 echo
 echo
 echo ".Tolls Ini Ada Beberapa Tools"
